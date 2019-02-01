a90af1fd32769d64491eef76247ec1545a95b9b8
Holden B <holden.bucher.92@gmail.com>
2019-02-01 10:27:03 -0600

    made db and started testing

--- api/server.js
+++ api/server.js
@@ -1,6 +1,18 @@
 const express = require('express');
 
+const db = require('../data/dbConfig');
+
 const server = express();
 server.use(express.json());
+
+server.post('/api/games', async (req, res) => {
+	const { title, genre } = req.body;
+	if (!title || !genre) {
+		res.status(422).end();
+	} else {
+		const added = await db('games').insert(req.body);
+		res.status(201).json(added);
+	}
+});
 
 module.exports = server;
--- api/server.spec.js
+++ api/server.spec.js
@@ -3,4 +3,31 @@ const server = require('./server');
 
 describe('GET /games endpoint', () => {});
 
-describe('POST /games endpoint', () => {});
+describe('POST /games endpoint', () => {
+	it('should send 422 if no title or genre', async () => {
+		let response = await request(server)
+			.post('/api/games')
+			.send({});
+
+		expect(response.status).toBe(422);
+
+		response = await request(server)
+			.post('/api/games')
+			.send({ title: 'Teenage Mutant Ninja Turtles' });
+
+		expect(response.status).toBe(422);
+
+		response = await request(server)
+			.post('/api/games')
+			.send({ genre: 'Puzzle' });
+
+		expect(response.status).toBe(422);
+	});
+	it('should send back 201 if added to database', async () => {
+		let response = await request(server)
+			.post('/api/games')
+			.send({ title: 'Teenage Mutant Ninja Turtles', genre: 'Fighting' });
+
+		expect(response.status).toBe(201);
+	});
+});
